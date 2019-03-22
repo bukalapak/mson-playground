629274163d5efacefc0843f687f258d9d64655df
Alif Rachmawadi <subosito@bukalapak.com>
2019-03-22 16:56:08 +0700

    Updates

---
+++ precache-manifest.27cfbbd8e31cbd3a6ef0f1127e683430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d51f2f6defa956cb9854",
+    "url": "/mson-playground/static/js/runtime~main.a541b3bb.js"
+  },
+  {
+    "revision": "c3e77e3328163ddf01fa",
+    "url": "/mson-playground/static/js/main.32878340.chunk.js"
+  },
+  {
+    "revision": "1c3e32f8d1fba87a2a5c",
+    "url": "/mson-playground/static/js/2.2041a134.chunk.js"
+  },
+  {
+    "revision": "c3e77e3328163ddf01fa",
+    "url": "/mson-playground/static/css/main.b775ae5d.chunk.css"
+  },
+  {
+    "revision": "1c3e32f8d1fba87a2a5c",
+    "url": "/mson-playground/static/css/2.1cf30d38.chunk.css"
+  },
+  {
+    "revision": "2864b41a758ea9f6a93c9233d567b74d",
+    "url": "/mson-playground/index.html"
+  }
+];
